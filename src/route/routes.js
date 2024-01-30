e0954dc14293cca1ed54752921f009288716294f
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-01-30 09:59:30 +0300

    feat: add CustomLink and fix routes

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,30 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
+import Header from "../components/Header/Header";
 
 export const PATHS = {
     HOME: "/",
     ABOUT: "/about",
     SIGNUP: "/signup",
     SIGNIN: "/signin",
+    HISTORY: "/history"
 };
 
-export const router = createBrowserRouter([
-    {
-        path: PATHS.HOME,
-        element: <Home />,
-    },
-    {
-        path: PATHS.ABOUT,
-        element: <div>about</div>,
-    },
-    {
-        path: PATHS.SIGNIN,
-        element: <SignIn />,
-    },
-    {
-        path: PATHS.SIGNUP,
-        element: <SignUp />,
-    },
-]);
+function CustomRouter() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={PATHS.HOME} element={<Home />} />
+                <Route path={PATHS.ABOUT} element={<div>about</div>} />
+                <Route path={PATHS.SIGNIN} element={<SignIn />} />
+                <Route path={PATHS.SIGNUP} element={<SignUp />} />
+                <Route path={PATHS.HISTORY} element={<div>HISTORY</div>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default CustomRouter;
