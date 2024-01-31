45bf81f8ea9cdf348d46bd835167a2335fd2a3ed
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-01-31 16:45:56 +0300

    feat: update routes

--- src/route/routes.js
+++ src/route/routes.js
@@ -3,13 +3,20 @@ import Home from "../pages/Home/Home";
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
 import Header from "../components/Header/Header";
+import SingleRecipe from "../components/SingleRecipe/SingleRecipe";
+import RecipeList from "../components/RecipeList/RecipeList";
+import NotFound from "../pages/ErrorPage/NotFound";
+
 
 export const PATHS = {
     HOME: "/",
-    ABOUT: "/about",
+    CATEGORY :"/category/:title",
+    RECIPE :"/recipe/:id",
     SIGNUP: "/signup",
     SIGNIN: "/signin",
+    FAVORITES: "/favorites",
     HISTORY: "/history"
+
 };
 
 function CustomRouter() {
@@ -18,10 +25,13 @@ function CustomRouter() {
             <Header />
             <Routes>
                 <Route path={PATHS.HOME} element={<Home />} />
-                <Route path={PATHS.ABOUT} element={<div>about</div>} />
+                <Route path={PATHS.CATEGORY} element={<RecipeList />} />
+                <Route path={PATHS.RECIPE} element={<SingleRecipe />} />
                 <Route path={PATHS.SIGNIN} element={<SignIn />} />
                 <Route path={PATHS.SIGNUP} element={<SignUp />} />
+                <Route path={PATHS.FAVORITES} element={<div>FAVORITES</div>} />
                 <Route path={PATHS.HISTORY} element={<div>HISTORY</div>} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
